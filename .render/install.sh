ca9fe514c2733687101766ff92989721fe6b4fec
drewfish <drew@folta.net>
2026-01-27 17:00:56 -0800

    render hosting basics

---
+++ .render/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# sync the dependecies into the Render cache
+uv sync --locked --no-dev
+
+# prune the uv cache so it doesn't grow indefinitely
+# https://docs.astral.sh/uv/concepts/cache/#caching-in-continuous-integration
+uv cache prune --ci
---
+++ .render/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# this will use dependecies restored from the Render cache
+uv run -- uvicorn --host 0.0.0.0 main:app
