7a30284ee2d0d936958d4f12b75dc59a83fe6b2e
drewfish <drew@folta.net>
2026-01-27 18:09:06 -0800

    debugging env vars

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Any
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,3 +8,9 @@ app = FastAPI()
 @app.get("/")
 def read_root(name: str = "World") -> Any:
     return {"message": f"Hello {name}"}
+
+@app.get("/drew/debug")
+def drew_debug() -> Any:
+    return {
+        "env": dict(os.environ)
+    }
