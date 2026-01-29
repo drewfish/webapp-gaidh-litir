e1f6442cbb90f8e461f5acfce37840828eb84d51
drewfish <drew@folta.net>
2026-01-28 22:11:08 -0800

    templating basics

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from typing import Any
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+)
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-def read_root(name: str = "World") -> Any:
-    return {"message": f"Hello {name}"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request, name: str = "World") -> Any:
+    context = {"name": name}
+    return templates.TemplateResponse(
+        request=request,
+        name="root.html",
+        context=context,
+    )
