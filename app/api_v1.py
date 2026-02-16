24903810cf81b381b3ef7b40e27811b354abf83e
drewfish <drew@folta.net>
2026-02-16 14:52:29 -0800

    removed debugging

--- app/api_v1.py
+++ app/api_v1.py
@@ -1,12 +1,14 @@
-from typing import Any
-import json
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
+
+# from typing import Any
+# import json
+# from fastapi import APIRouter, Request
 
 router = APIRouter(prefix="/api/v1")
 
 
-@router.post("/debug")
-async def debug(request: Request) -> Any:
-    payload = await request.json()
-    print("--DEBUG-REMOTE--", json.dumps(payload, indent=2))
-    return ""
+#   @router.post("/debug")
+#   async def debug(request: Request) -> Any:
+#       payload = await request.json()
+#       print("--DEBUG-REMOTE--", json.dumps(payload, indent=2))
+#       return ""
